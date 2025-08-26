90b7ad63e917405e1f6229cea32a78610011196c
daon24 <adfs80342@gmail.com>
2025-08-26 16:01:55 +0900

    자바의 정석 4과 연습문제 풀이

---
+++ src/EX4_12.java
@@ -0,0 +1,10 @@
+//public class EX4_12 {
+//    public static void main(String[] args) {
+//        Outer: for(int i=1; i<=9; i++){
+//            for(int j=2; j<=9; j++)
+//            {if()
+//                System.out.printf("%d*%d=%d \t",j,i,j*i);
+//            }
+//        }
+//    }
+//}
--- src/EX4_9.java
+++ src/EX4_9.java
@@ -1,2 +1,13 @@
 public class EX4_9 {
+    public static void main(String[] args) {
+        String str = "12345";
+        int sum = 0;
+        for(int i=0; i< str.length(); i++){
+            char num = str.charAt(i);
+            int num2 = num - '0';
+            sum += num2;
+        }
+        System.out.println(sum);
+        }
+
 }
